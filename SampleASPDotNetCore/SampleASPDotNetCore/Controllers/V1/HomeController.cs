0865d89d26e534e67c2c30059129471f92ee7916
sudhakar.tumma <sudhakar.tumma@ascendlearning.com>
2025-05-17 17:36:46 +0530

    Added swagger in the program file

---
+++ SampleASPDotNetCore/SampleASPDotNetCore/Controllers/V1/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SampleASPDotNetCore.Controllers.V1
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        [Route("item")]
+        public IActionResult Index()
+        {
+            return Ok("string");
+        }
+    }
+}
--- SampleASPDotNetCore/SampleASPDotNetCore/Program.cs
+++ SampleASPDotNetCore/SampleASPDotNetCore/Program.cs
@@ -1,6 +1,19 @@
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "Sample API");            
+            c.RoutePrefix = "api/swagger";
+        });
+app.MapControllers();// Required to map the controllers else http methods will not be found
 
 app.Run();
